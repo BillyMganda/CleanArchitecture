4e6496de7b8390ecc365cf03110c12a68faef262
Billy <bimganda@gmail.com>
2023-03-20 16:59:33 -0400

    added DeleteCustomerCommand in Application.Commands

---
+++ CleanArchitecture.Application/Commands/DeleteCustomerCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Commands
+{
+    public class DeleteCustomerCommand : IRequest<string>
+    {
+        public Guid Id { get; private set; }
+
+        public DeleteCustomerCommand(Guid Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
