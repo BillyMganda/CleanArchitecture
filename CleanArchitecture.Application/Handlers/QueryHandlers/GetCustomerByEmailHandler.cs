c7cd25d5be2dde71171a487cfc98b60ba4c13f41
Billy <bimganda@gmail.com>
2023-03-20 17:25:47 -0400

    added GetCustomerByEmailHandler in Application.Handlers.QueryHandlers

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/GetCustomerByEmailHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Queries;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers
+{
+    public class GetCustomerByEmailHandler : IRequestHandler<GetCustomerByEmailQuery, Customer>
+    {
+        private readonly IMediator _mediator;
+
+        public GetCustomerByEmailHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<Customer> Handle(GetCustomerByEmailQuery request, CancellationToken cancellationToken)
+        {
+            var customers = await _mediator.Send(new GetAllCustomerQuery());
+            var selectedCustomer = customers.FirstOrDefault(x => x.Email.ToLower().Contains(request.Email.ToLower()));
+            return selectedCustomer!;
+        }
+    }
+}
