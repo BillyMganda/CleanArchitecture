e0e4d52348b6149769a74cf91a77ade37be1e5dd
Billy <bimganda@gmail.com>
2023-03-20 17:04:31 -0400

    added GetCustomerByIdQuery in Application.Queries

---
+++ CleanArchitecture.Application/Queries/GetCustomerByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries
+{
+    public class GetCustomerByIdQuery : IRequest<Customer>
+    {
+        public Guid Id { get; private set; }
+
+        public GetCustomerByIdQuery(Guid Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
