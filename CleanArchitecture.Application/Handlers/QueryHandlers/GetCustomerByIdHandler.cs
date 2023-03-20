9a1ba11021b49125589f0778837cd54eb8caacd2
Billy <bimganda@gmail.com>
2023-03-20 17:27:35 -0400

    added GetCustomerByIdHandler in Application.Handlers.QueryHandlers

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/GetCustomerByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Queries;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers
+{
+    public class GetCustomerByIdHandler : IRequestHandler<GetCustomerByIdQuery, Customer>
+    {
+        private readonly IMediator _mediator;
+
+        public GetCustomerByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<Customer> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customers = await _mediator.Send(new GetAllCustomerQuery());
+            var selectedCustomer = customers.FirstOrDefault(x => x.Id == request.Id);
+            return selectedCustomer!;
+        }
+    }
+}
