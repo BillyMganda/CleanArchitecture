cfb56edf92eb8abf1fceffeeacf1497740fa1024
Billy <bimganda@gmail.com>
2023-03-20 17:09:02 -0400

    added OrderingMappingProfile in Application.Mapper

--- CleanArchitecture.Application/Mapper/CustomerMapper.cs
+++ CleanArchitecture.Application/Mapper/CustomerMapper.cs
@@ -8,7 +8,7 @@ namespace CleanArchitecture.Application.Mapper
         {
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.ShouldMapProperty = p => p.GetMethod!.IsPublic || p.GetMethod.IsAssembly;
                 cfg.AddProfile<OrderingMappingProfile>();
             });
 
---
+++ CleanArchitecture.Application/Mapper/OrderingMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Application.Response;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Mapper
+{
+    public class OrderingMappingProfile : Profile
+    {
+        public OrderingMappingProfile()
+        {
+            CreateMap<Customer, CustomerResponse>().ReverseMap();
+            CreateMap<Customer, CreateCustomerCommand>().ReverseMap();
+            CreateMap<Customer, EditCustomerCommand>().ReverseMap();
+        }
+    }
+}
