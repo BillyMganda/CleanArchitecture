851df802fd24ece12bcf9693bd16c92a7105dbb1
Billy <bimganda@gmail.com>
2023-03-20 17:01:21 -0400

    added GetAllCustomerQuery in Application.Queries

---
+++ CleanArchitecture.Application/Queries/GetAllCustomerQuery.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries
+{
+    public class GetAllCustomerQuery : IRequest<List<Customer>>
+    {
+    }
+}
