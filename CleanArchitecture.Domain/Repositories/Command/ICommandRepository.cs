da7620c3b9ab4a8745b7806368f18c59614c2c30
Billy <bimganda@gmail.com>
2023-03-20 11:14:59 -0400

    added ICommandRepository interface

--- CleanArchitecture.Domain/Repositories/Command/ICommandRepository.cs
+++ CleanArchitecture.Domain/Repositories/Command/ICommandRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace CleanArchitecture.Domain.Repositories.Command
+﻿using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Repositories.Command
 {
-    public interface ICommandRepository
+    public interface ICommandRepository<T> where T : class
     {
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
