c3dc8df2c8dbb37af04e4ceb06b11e51189b7cd2
Billy <bimganda@gmail.com>
2023-03-20 17:20:08 -0400

    created EditCustomerHandler in Application.Handlers.CommandHandlers

---
+++ CleanArchitecture.Application/Handlers/CommandHandlers/DeleteCustomerHandler.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.CommandHandlers
+{
+    public class DeleteCustomerHandler : IRequestHandler<DeleteCustomerCommand, string>
+    {
+        private readonly ICustomerCommandRepository _customerCommandRepository;
+        private readonly ICustomerQueryRepository _customerQueryRepository;
+        public DeleteCustomerHandler(ICustomerCommandRepository customerRepository, ICustomerQueryRepository customerQueryRepository)
+        {
+            _customerCommandRepository = customerRepository;
+            _customerQueryRepository = customerQueryRepository;
+        }
+
+        public async Task<string> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var customerEntity = await _customerQueryRepository.GetByIdAsync(request.Id);
+
+                await _customerCommandRepository.DeleteAsync(customerEntity);
+            }
+            catch (Exception exp)
+            {
+                throw (new ApplicationException(exp.Message));
+            }
+
+            return "Customer information has been deleted!";
+        }
+    }
+}
---
+++ CleanArchitecture.Application/Handlers/CommandHandlers/EditCustomerHandler.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Application.Mapper;
+using CleanArchitecture.Application.Response;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.CommandHandlers
+{
+    public class EditCustomerHandler : IRequestHandler<EditCustomerCommand, CustomerResponse>
+    {
+        private readonly ICustomerCommandRepository _customerCommandRepository;
+        private readonly ICustomerQueryRepository _customerQueryRepository;
+        public EditCustomerHandler(ICustomerCommandRepository customerRepository, ICustomerQueryRepository customerQueryRepository)
+        {
+            _customerCommandRepository = customerRepository;
+            _customerQueryRepository = customerQueryRepository;
+        }
+
+        public async Task<CustomerResponse> Handle(EditCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customerEntity = CustomerMapper.Mapper.Map<Customer>(request);
+             
+            if (customerEntity is null)
+            {
+                throw new ApplicationException("There is a problem in mapper");
+            }
+
+            try
+            {
+                await _customerCommandRepository.UpdateAsync(customerEntity);
+            }
+            catch (Exception exp)
+            {
+                throw new ApplicationException(exp.Message);
+            }
+
+            var modifiedCustomer = await _customerQueryRepository.GetByIdAsync(request.Id);
+            var customerResponse = CustomerMapper.Mapper.Map<CustomerResponse>(modifiedCustomer);
+
+            return customerResponse;
+        }
+    }
+}
