867b6df98e430614426cacd47cf54acca290f8f2
Billy <bimganda@gmail.com>
2023-03-20 16:50:24 -0400

    added CustomerResponse in Domain.Response

---
+++ CleanArchitecture.Domain/Response/CustomerResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchitecture.Domain.Response
+{
+    public class CustomerResponse
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Repository/Query/CustomerQueryRepository.cs
@@ -0,0 +1,72 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Query;
+using CleanArchitecture.Infrastructure.Repository.Query.Base;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Query
+{
+    public class CustomerQueryRepository : QueryRepository<Customer>, ICustomerQueryRepository
+    {
+        public CustomerQueryRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public async Task<IReadOnlyList<Customer>> GetAllAsync()
+        {
+            try
+            {
+                var query = "SELECT * FROM CUSTOMERS";
+
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryAsync<Customer>(query)).ToList();
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+
+        public async Task<Customer> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var query = "SELECT * FROM CUSTOMERS WHERE Id = @Id";
+                var parameters = new DynamicParameters();
+                parameters.Add("Id", id, DbType.Guid);
+
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryFirstOrDefaultAsync<Customer>(query, parameters));
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+
+        public async Task<Customer> GetCustomerByEmail(string email)
+        {
+            try
+            {
+                var query = "SELECT * FROM CUSTOMERS WHERE Email = @email";
+                var parameters = new DynamicParameters();
+                parameters.Add("Email", email, DbType.String);
+
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryFirstOrDefaultAsync<Customer>(query, parameters));
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
+        }
+    }
+}
