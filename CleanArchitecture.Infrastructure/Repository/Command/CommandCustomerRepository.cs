392c4f7f2f465d12a1aff8fcc3491e579c1d6b33
Billy <bimganda@gmail.com>
2023-03-20 15:13:52 -0400

    added QueryRepository in Infrastructure.Repository.Query.Base

---
+++ CleanArchitecture.Infrastructure/Repository/Command/CommandCustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Command
+{
+    public class CommandCustomerRepository : CommandRepository<Customer>, ICustomerCommandRepository
+    {
+        public CustomerCommandRepository(OrderingContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Repository/Query/Base/QueryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Repositories.Query;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArchitecture.Infrastructure.Repository.Query.Base
+{
+    public class QueryRepository<T> : DbConnector,  IQueryRepository<T> where T : class
+    {
+        public QueryRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+    }
+}
