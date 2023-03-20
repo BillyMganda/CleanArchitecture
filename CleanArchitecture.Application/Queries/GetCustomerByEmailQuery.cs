4e05dfff58d113b95b08cf3bf6ebdd1cf4d6711e
Billy <bimganda@gmail.com>
2023-03-20 17:02:45 -0400

    added GetCustomerByEmailQuery in Application.Queries

---
+++ CleanArchitecture.Application/Queries/GetCustomerByEmailQuery.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries
+{
+    public class GetCustomerByEmailQuery : IRequest<Customer>
+    {
+        public string Email { get; private set; }
+
+        public GetCustomerByEmailQuery(string email)
+        {
+            Email = email;
+        }
+    }
+}
