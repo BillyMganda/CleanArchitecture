d1e2e4cd669948a1e4334b337563852c1486cba4
Billy <bimganda@gmail.com>
2023-03-21 10:43:59 -0400

    added xunit project with Get_ReturnsListOfCustomers test

---
+++ CleanArchitecture.Test/CustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitecture.API.Controllers;
+using CleanArchitecture.Application.Queries;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+using Moq;
+
+namespace CleanArchitecture.Test
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public async Task Get_ReturnsListOfCustomers()
+        {
+            // Arrange
+            var expectedCustomers = new List<Customer> { new Customer(), new Customer() };
+
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock
+                .Setup(x => x.Send(It.IsAny<GetAllCustomerQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedCustomers);
+
+            var controller = new customersController(mediatorMock.Object);
+
+            // Act
+            var result = await controller.Get();
+
+            // Assert
+            Assert.IsType<List<Customer>>(result);
+            Assert.Equal(expectedCustomers, result);
+        }
+    }
+}
