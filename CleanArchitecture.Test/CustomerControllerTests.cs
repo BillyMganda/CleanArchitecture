c380de1a697472cb126ee439e62b84f97d6e4fe1
Billy <bimganda@gmail.com>
2023-03-21 11:04:09 -0400

    created a passing test for Get_WithValidCustomerId_ReturnsCustomer

--- CleanArchitecture.Test/CustomerControllerTests.cs
+++ CleanArchitecture.Test/CustomerControllerTests.cs
@@ -8,25 +8,49 @@ namespace CleanArchitecture.Test
 {
     public class CustomerControllerTests
     {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly customersController _customerController;
+        public CustomerControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _customerController = new customersController(_mediatorMock.Object);
+        }
         [Fact]
         public async Task Get_ReturnsListOfCustomers()
         {
             // Arrange
             var expectedCustomers = new List<Customer> { new Customer(), new Customer() };
-
-            var mediatorMock = new Mock<IMediator>();
-            mediatorMock
-                .Setup(x => x.Send(It.IsAny<GetAllCustomerQuery>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(expectedCustomers);
-
-            var controller = new customersController(mediatorMock.Object);
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetAllCustomerQuery>(), default)).ReturnsAsync(expectedCustomers);
 
             // Act
-            var result = await controller.Get();
+            var result = await _customerController.Get();
 
             // Assert
             Assert.IsType<List<Customer>>(result);
-            Assert.Equal(expectedCustomers, result);
+            Assert.Equal(expectedCustomers.Count, result.Count);
+            for (int i = 0; i < expectedCustomers.Count; i++)
+            {
+                Assert.Equal(expectedCustomers[i].Id, result[i].Id);
+                Assert.Equal(expectedCustomers[i].FirstName, result[i].FirstName);
+                Assert.Equal(expectedCustomers[i].LastName, result[i].LastName);
+            }
+        }
+
+        [Fact]
+        public async Task Get_WithValidCustomerId_ReturnsCustomer()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            var expectedCustomer = new Customer { Id = customerId, FirstName = "John" };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetCustomerByIdQuery>(), default)).ReturnsAsync(expectedCustomer);
+
+            // Act
+            var result = await _customerController.Get(customerId);
+
+            // Assert
+            Assert.IsType<Customer>(result);
+            Assert.Equal(expectedCustomer.Id, result.Id);
+            Assert.Equal(expectedCustomer.FirstName, result.FirstName);
         }
     }
 }
