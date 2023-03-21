6b0c1c6c1e10a34acb8c7b2bafe494abf6266bc3
Billy <bimganda@gmail.com>
2023-03-21 17:25:48 -0400

    implemented IBrandCommandRepository with BrandCommandRepository in Infrastructure.Repository.Command

---
+++ CleanArchitecture.Infrastructure/Repository/Command/BrandCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Command;
+using CleanArchitecture.Infrastructure.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Command
+{
+    public class BrandCommandRepository : CommandRepository<Brand>, ICommandBrandRepository
+    {
+        public BrandCommandRepository(OrderingContext context) : base(context)
+        {
+
+        }
+    }
+}
