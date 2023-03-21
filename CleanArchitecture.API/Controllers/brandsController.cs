4ee5f6030b1a90cda40e0e587c2d62b6207dc0de
Billy <bimganda@gmail.com>
2023-03-21 17:32:17 -0400

    implemented brandsController

---
+++ CleanArchitecture.API/Controllers/brandsController.cs
@@ -0,0 +1,80 @@
+﻿using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Application.Commands.Brands;
+using CleanArchitecture.Application.Queries;
+using CleanArchitecture.Application.Queries.Brands;
+using CleanArchitecture.Application.Response;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class brandsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public brandsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<Brand>> Get()
+        {
+            return await _mediator.Send(new GetAllBrandQuery());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<Brand> Get(Guid id)
+        {
+            return await _mediator.Send(new GetBrandByIdQuery(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<BrandResponse>> CreateBrand([FromBody] CreateBrandCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditBrand(Guid id, [FromBody] EditBrandCommand command)
+        {
+            try
+            {
+                if (command.Id == id)
+                {
+                    var result = await _mediator.Send(command);
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBrand(Guid id)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = await _mediator.Send(new DeleteBrandCommand(id));
+                return Ok(result);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+    }
+}
