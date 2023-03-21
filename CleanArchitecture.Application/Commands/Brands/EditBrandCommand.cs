fb6484e2bb0903e32187266b0231ee472499df57
Billy <bimganda@gmail.com>
2023-03-21 16:46:29 -0400

    added EditBrandCommand in Application.Commands.Brands

---
+++ CleanArchitecture.Application/Commands/Brands/EditBrandCommand.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Application.Response;
+using MediatR;
+
+namespace CleanArchitecture.Application.Commands.Brands
+{
+    public class EditBrandCommand : IRequest<BrandResponse>
+    {
+        public Guid Id { get; set; }
+        public string BrandName { get; set; } = string.Empty;
+    }
+}
