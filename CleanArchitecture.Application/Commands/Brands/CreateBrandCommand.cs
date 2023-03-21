b95c5aff75cc4e5af01141d07e5ae7a08a4aa515
Billy <bimganda@gmail.com>
2023-03-21 16:44:23 -0400

    added DeleteBrandCommand in  Application.Commands.Brands

--- CleanArchitecture.Application/Commands/Brands/CreateBrandCommand.cs
+++ CleanArchitecture.Application/Commands/Brands/CreateBrandCommand.cs
@@ -5,11 +5,13 @@ namespace CleanArchitecture.Application.Commands.Brands
 {
     public class CreateBrandCommand : IRequest<BrandResponse>
     {
-        public string BrandName { get; set; }
+        public string BrandName { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
         public CreateBrandCommand()
         {
             CreatedDate = DateTime.Now;
+            ModifiedDate = DateTime.Now;
         }
     }
 }
---
+++ CleanArchitecture.Application/Commands/Brands/DeleteBrandCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Commands.Brands
+{
+    public class DeleteBrandCommand : IRequest<string>
+    {
+        public Guid Id { get; private set; }
+        public DeleteBrandCommand(Guid Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
