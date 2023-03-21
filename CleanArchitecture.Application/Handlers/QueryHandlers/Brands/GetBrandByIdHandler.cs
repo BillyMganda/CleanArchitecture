04d83c7845a6a937f9d9954ac7da592dbae0b762
Billy <bimganda@gmail.com>
2023-03-21 16:54:41 -0400

    implemented GetBrandByIdHandler in Application.Handlers.QueryHandlers.Brands

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/Brands/GetBrandByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Queries.Brands;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers.Brands
+{
+    public class GetBrandByIdHandler : IRequestHandler<GetBrandByIdQuery, Brand>
+    {
+        private readonly IMediator _mediator;
+        public GetBrandByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Brand> Handle(GetBrandByIdQuery request, CancellationToken cancellationToken)
+        {
+            var brands = await _mediator.Send(new GetAllBrandQuery());
+            var selectedBrand = brands.FirstOrDefault(x => x.Id == request.Id);
+            return selectedBrand!;
+        }
+    }
+}
