79b1ca55b23189ca104ed1ae4bd215607237690e
Billy <bimganda@gmail.com>
2023-03-21 16:50:03 -0400

    implemented GetAllBrandHandler in Application.Handlers.QueryHandlers.Brands

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/Brands/GetAllBrandHandler.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Queries.Brands;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers.Brands
+{
+    public class GetAllBrandHandler : IRequestHandler<GetAllBrandQuery, List<Brand>>
+    {
+        private readonly IBrandQueryRepository _brandQueryRepository;
+        public GetAllBrandHandler(IBrandQueryRepository brandQueryRepository)
+        {
+            _brandQueryRepository = brandQueryRepository;
+        }
+
+        public async Task<List<Brand>> Handle(GetAllBrandQuery request, CancellationToken cancellationToken)
+        {
+            return (List<Brand>)await _brandQueryRepository.GetAllAsync();
+        }
+    }
+}
