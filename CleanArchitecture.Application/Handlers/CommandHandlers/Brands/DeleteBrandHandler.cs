47b2e4b69cc882b1c27e279412f844aedd3910da
Billy <bimganda@gmail.com>
2023-03-21 17:09:50 -0400

    implemented DeleteBrandHandler in Application.Handlers.CommandHandlers.Brands

---
+++ CleanArchitecture.Application/Handlers/CommandHandlers/Brands/DeleteBrandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchitecture.Application.Commands.Brands;
+using CleanArchitecture.Domain.Repositories.Command;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.CommandHandlers.Brands
+{
+    public class DeleteBrandHandler : IRequestHandler<DeleteBrandCommand, string>
+    {
+        private readonly ICommandBrandRepository _brandCommandRepository;
+        private readonly IBrandQueryRepository _brandQueryRepository;
+        public DeleteBrandHandler(ICommandBrandRepository brandCommandRepository, IBrandQueryRepository brandQueryRepository)
+        {
+            _brandCommandRepository = brandCommandRepository;
+            _brandQueryRepository = brandQueryRepository;
+        }
+
+        public async Task<string> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var brandEntity = await _brandQueryRepository.GetByIdAsync(request.Id);
+
+                await _brandCommandRepository.DeleteAsync(brandEntity);
+            }
+            catch (Exception exp)
+            {
+                throw (new ApplicationException(exp.Message));
+            }
+
+            return "Brand information has been deleted!";
+        }
+    }
+}
