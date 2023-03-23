4d1c330ff4f485be35cc8304023309fc54d3e3b7
Billy <bimganda@gmail.com>
2023-03-23 14:29:23 -0400

    added GetCategoryByIdQuery in Application.Queries.Categories

---
+++ CleanArchitecture.Application/Queries/Categories/GetCategoryByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries.Categories
+{
+    public class GetCategoryByIdQuery : IRequest<Category>
+    {
+        public Guid Id { get; private set; }
+        public GetCategoryByIdQuery(Guid Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
