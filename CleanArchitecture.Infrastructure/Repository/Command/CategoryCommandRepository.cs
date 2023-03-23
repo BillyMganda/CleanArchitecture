cfb78ed4651966c982694089a8068fc261216b32
Billy <bimganda@gmail.com>
2023-03-23 15:10:16 -0400

    implemented CategoryCommandRepository

---
+++ CleanArchitecture.Infrastructure/Repository/Command/CategoryCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Command;
+using CleanArchitecture.Infrastructure.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Command
+{
+    public class CategoryCommandRepository : CommandRepository<Category>, ICommandCategoryRepository
+    {
+        public CategoryCommandRepository(OrderingContext context) : base(context)
+        {
+
+        }
+    }
+}
