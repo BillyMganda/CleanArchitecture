4ffd3ce9442be4fdf369953510cc411071968cf3
Billy <bimganda@gmail.com>
2023-03-23 15:16:03 -0400

    implemented categoriesController

---
+++ CleanArchitecture.API/Controllers/categoriesController.cs
@@ -0,0 +1,78 @@
+﻿using CleanArchitecture.Application.Commands.Categories;
+using CleanArchitecture.Application.Queries.Categories;
+using CleanArchitecture.Application.Response;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class categoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public categoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<Category>> Get()
+        {
+            return await _mediator.Send(new GetAllCategoryQuery());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<Category> Get(Guid id)
+        {
+            return await _mediator.Send(new GetCategoryByIdQuery(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CategoryResponse>> CreateCategory([FromBody] CreateCategoryCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditCategory(Guid id, [FromBody] EditCategoryCommand command)
+        {
+            try
+            {
+                if (command.Id == id)
+                {
+                    var result = await _mediator.Send(command);
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCategory(Guid id)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = await _mediator.Send(new DeleteCategoryCommand(id));
+                return Ok(result);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+    }
+}
