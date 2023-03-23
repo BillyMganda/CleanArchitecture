908221b104213e4cfd603fc3f1e721d17852cd94
Billy <bimganda@gmail.com>
2023-03-23 14:40:57 -0400

    added EditCategoryCommand in Application.Commands.Categories

---
+++ CleanArchitecture.Application/Commands/Categories/EditCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Application.Response;
+using MediatR;
+
+namespace CleanArchitecture.Application.Commands.Categories
+{
+    public class EditCategoryCommand : IRequest<CategoryResponse>
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
