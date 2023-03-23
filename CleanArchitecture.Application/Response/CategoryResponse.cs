769ac57c32d1acba91a6aa8e4ac181c33a035f1f
Billy <bimganda@gmail.com>
2023-03-23 14:25:01 -0400

    added CategoryResponse class in Application.Response

---
+++ CleanArchitecture.Application/Response/CategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Application.Response
+{
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
