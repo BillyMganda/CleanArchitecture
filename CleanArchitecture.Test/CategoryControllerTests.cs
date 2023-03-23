ce14faf76fb88512d1c156ced79c46ee3c39c6e6
Billy <bimganda@gmail.com>
2023-03-23 18:39:10 -0400

    create Get_WithValidCategoryId_ReturnsCategory

--- CleanArchitecture.Test/CategoryControllerTests.cs
+++ CleanArchitecture.Test/CategoryControllerTests.cs
@@ -38,5 +38,22 @@ namespace CleanArchitecture.Test
                 Assert.Equal(expectedCategories[i].ModifiedDate, result[i].ModifiedDate);
             }
         }
+
+        [Fact]
+        public async Task Get_WithValidCategoryId_ReturnsCategory()
+        {
+            // Arrange
+            var categoryId = Guid.NewGuid();
+            var expectedCategory = new Category { Id = categoryId, CategoryName = "Duis ac" };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetCategoryByIdQuery>(), default)).ReturnsAsync(expectedCategory);
+
+            // Act
+            var result = await _categoriesController.Get(categoryId);
+
+            // Assert
+            Assert.IsType<Category>(result);
+            Assert.Equal(expectedCategory.Id, result.Id);
+            Assert.Equal(expectedCategory.CategoryName, result.CategoryName);
+        }
     }
 }
