762e7a51a6c10982e2090832f5590c9f4b188f0a
Billy <bimganda@gmail.com>
2023-03-23 14:39:25 -0400

    created DeleteCategoryCommand in Application.Commands.Categories

---
+++ CleanArchitecture.Application/Commands/Categories/DeleteCategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Commands.Categories
+{
+    public class DeleteCategoryCommand : IRequest<string>
+    {
+        public Guid Id { get; private set; }
+        public DeleteCategoryCommand(Guid Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
