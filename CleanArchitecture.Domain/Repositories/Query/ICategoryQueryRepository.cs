f235a4e731d516dd826abbc99c15c731967c8935
Billy <bimganda@gmail.com>
2023-03-23 14:18:35 -0400

    added ICategoryQueryRepository interface

---
+++ CleanArchitecture.Domain/Repositories/Query/ICategoryQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Repositories.Query
+{
+    public interface ICategoryQueryRepository : IQueryRepository<Category>
+    {
+        // Custom operation which is not generic
+        Task<IReadOnlyList<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(Guid id);
+    }
+}
