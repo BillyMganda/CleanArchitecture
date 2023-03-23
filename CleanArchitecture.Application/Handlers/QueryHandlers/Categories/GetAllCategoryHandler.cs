a6e380d7772c4af10ca8a17d25a961254aeca6bc
Billy <bimganda@gmail.com>
2023-03-23 14:44:29 -0400

    added GetAllCategoryHandler in Application.Handlers.QueryHandlers.Categories

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/Categories/GetAllCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Queries.Categories;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers.Categories
+{
+    public class GetAllCategoryHandler : IRequestHandler<GetAllCategoryQuery, List<Category>>
+    {
+        private readonly ICategoryQueryRepository _categoryQueryRepository;
+        public GetAllCategoryHandler(ICategoryQueryRepository categoryQueryRepository)
+        {
+            _categoryQueryRepository = categoryQueryRepository;
+        }
+
+        public async Task<List<Category>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            return (List<Category>)await _categoryQueryRepository.GetAllAsync();
+        }
+    }
+}
