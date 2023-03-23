be027dc86be069edd5ec61a2301dae9eff913348
Billy <bimganda@gmail.com>
2023-03-23 14:48:00 -0400

    added GetCategoryByIdHandler to handle GetCategoryByIdQuery

---
+++ CleanArchitecture.Application/Handlers/QueryHandlers/Categories/GetCategoryByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Queries.Categories;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.QueryHandlers.Categories
+{
+    public class GetCategoryByIdHandler : IRequestHandler<GetCategoryByIdQuery, Category>
+    {
+        private readonly IMediator _mediator;
+        public GetCategoryByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Category> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await _mediator.Send(new GetAllCategoryQuery());
+            var selectedCategory = categories.FirstOrDefault(x => x.Id == request.Id);
+            return selectedCategory!;
+        }
+    }
+}
