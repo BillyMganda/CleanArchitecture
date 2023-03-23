067b0b325696d3f5a711627ebf860e5ba88d1f22
Billy <bimganda@gmail.com>
2023-03-23 14:27:12 -0400

    added GetAllCategoryQuery in Application.Queries.Categories

---
+++ CleanArchitecture.Application/Queries/Categories/GetAllCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries.Categories
+{
+    public class GetAllCategoryQuery : IRequest<List<Category>>
+    {
+    }
+}
