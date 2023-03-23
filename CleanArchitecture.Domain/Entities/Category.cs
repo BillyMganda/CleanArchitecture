bfbd9a4c3f5ec90ae2facb6957b018f4e0ee3e06
Billy <bimganda@gmail.com>
2023-03-23 14:14:01 -0400

    created category class

---
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities.Base;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
