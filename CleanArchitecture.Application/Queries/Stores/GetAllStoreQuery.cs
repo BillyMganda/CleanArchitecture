25c3bdb9372a73c57ab49c2a115838b0db572ca1
Billy <bimganda@gmail.com>
2023-03-24 14:30:50 -0400

    add GetAllStoreQuery that uses MediatR

---
+++ CleanArchitecture.Application/Queries/Stores/GetAllStoreQuery.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries.Stores
+{
+    public class GetAllStoreQuery : IRequest<List<Store>>
+    {
+    }
+}
