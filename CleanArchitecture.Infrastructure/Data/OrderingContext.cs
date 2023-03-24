1b8e3ba27f66727b63e7c82c343a7fcf55908f3d
Billy <bimganda@gmail.com>
2023-03-24 15:28:18 -0400

    update db context to include Stores table

--- CleanArchitecture.Infrastructure/Data/OrderingContext.cs
+++ CleanArchitecture.Infrastructure/Data/OrderingContext.cs
@@ -13,5 +13,6 @@ namespace CleanArchitecture.Infrastructure.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Store> Stores { get; set; }
     }
 }
