1b2b66f6e086297c25caf0e507d332c0b94f85e1
Billy <bimganda@gmail.com>
2023-03-24 13:21:39 -0400

    create StoreResponse class

---
+++ CleanArchitecture.Application/Response/StoreResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchitecture.Application.Response
+{
+    public class StoreResponse
+    {
+        public Guid Id { get; set; }
+        public string StoreName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
