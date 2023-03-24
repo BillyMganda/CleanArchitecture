6e2b225edc2ff541b6e2cde740a515394abfb1c3
Billy <bimganda@gmail.com>
2023-03-24 14:44:22 -0400

    create command to edit store details, return StoreResponse

---
+++ CleanArchitecture.Application/Commands/Stores/EditStoreCommand.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Application.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Application.Commands.Stores
+{
+    public class EditStoreCommand : IRequest<StoreResponse>
+    {
+        public string StoreName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+        public DateTime ModifiedDate { get; set; }
+        public EditStoreCommand()
+        {
+            ModifiedDate = DateTime.Now;
+        }
+    }
+}
