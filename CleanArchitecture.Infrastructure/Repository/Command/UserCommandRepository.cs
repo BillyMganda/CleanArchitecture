32f8f279d8eff4769df3038f7c10114b81fd0e21
Billy <bimganda@gmail.com>
2023-03-24 19:48:10 -0400

    create implementation of user command repository

---
+++ CleanArchitecture.Infrastructure/Repository/Command/UserCommandRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Infrastructure.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Command
+{
+    public class UserCommandRepository
+    {
+        protected readonly OrderingContext _context;
+        public UserCommandRepository(OrderingContext context)
+        {
+            _context = context;
+        }
+    }
+}
