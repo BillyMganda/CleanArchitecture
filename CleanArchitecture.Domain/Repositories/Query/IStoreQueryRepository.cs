3bdef976eb793748251bc1c7ecd02410035557ce
Billy <bimganda@gmail.com>
2023-03-24 13:03:13 -0400

    add IStoreQueryRepository that inherits from IQueryRepository

---
+++ CleanArchitecture.Domain/Repositories/Query/IStoreQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Repositories.Query
+{
+    public interface IStoreQueryRepository : IQueryRepository<Store>
+    {
+        // Custom operation which is not generic
+        Task<IReadOnlyList<Store>> GetAllAsync();
+        Task<Store> GetByIdAsync(Guid id);
+    }
+}
