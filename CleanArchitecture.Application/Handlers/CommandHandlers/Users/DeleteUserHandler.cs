fb7689ab82b969effa6fe21d76c4ca26c82e8a4f
Billy <bimganda@gmail.com>
2023-03-24 19:24:09 -0400

    add command to delete user info

---
+++ CleanArchitecture.Application/Handlers/CommandHandlers/Users/DeleteUserHandler.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Application.Commands.Users;
+using CleanArchitecture.Domain.Repositories.Command;
+using CleanArchitecture.Domain.Repositories.Query;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Handlers.CommandHandlers.Users
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, string>
+    {
+        private readonly ICommandUserRepository _userCommandRepository;
+        private readonly IUserQueryRepository _userQueryRepository;
+        public DeleteUserHandler(ICommandUserRepository userCommandRepository, IUserQueryRepository userQueryRepository)
+        {
+            _userCommandRepository = userCommandRepository;
+            _userQueryRepository = userQueryRepository;
+        }
+
+        public async Task<string> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = await _userQueryRepository.GetByIdAsync(request.Id);
+
+                await _userCommandRepository.DeleteAsync(entity);
+            }
+            catch (Exception exp)
+            {
+                throw (new ApplicationException(exp.Message));
+            }
+
+            return "User information has been deleted!";
+        }
+    }
+}
