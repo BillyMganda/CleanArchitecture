69e4020744f365b1666717fdbcd679b228b28f51
Billy <bimganda@gmail.com>
2023-03-24 15:42:09 -0400

    create storesController with MediatR DI in its constractor

---
+++ CleanArchitecture.API/Controllers/storesController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class storesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public storesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
