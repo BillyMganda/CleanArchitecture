c78c48afbc06c40ac3d20ba55068c4da35189fbe
Billy <bimganda@gmail.com>
2023-03-24 19:02:34 -0400

    add command to delete user

---
+++ CleanArchitecture.Application/Commands/Users/DeleteUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Commands.Users
+{
+    public class DeleteUserCommand : IRequest<string>
+    {
+        public Guid Id { get; private set; }
+        public DeleteUserCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
