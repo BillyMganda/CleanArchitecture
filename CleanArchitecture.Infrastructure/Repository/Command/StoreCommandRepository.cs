d69f4a02f40d8480b6bc848b0a01ea1f47fbb3a5
Billy <bimganda@gmail.com>
2023-03-24 15:39:38 -0400

    implement ICommandStoreRepository in infra layer

---
+++ CleanArchitecture.Infrastructure/Repository/Command/StoreCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories.Command;
+using CleanArchitecture.Infrastructure.Data;
+
+namespace CleanArchitecture.Infrastructure.Repository.Command
+{
+    public class StoreCommandRepository : CommandRepository<Store>, ICommandStoreRepository
+    {
+        public StoreCommandRepository(OrderingContext context) : base(context)
+        {
+
+        }
+    }
+}
