81cd5675a5a20dcab2f7efbb025e21aed1ef661c
Billy <bimganda@gmail.com>
2023-03-30 12:50:33 -0400

    add command to modify a user, should return GetUserDto object

---
+++ CleanArchitecture.Application/Commands/ModifyUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.DTOs.Users;
+using MediatR;
+
+namespace CleanArchitecture.Application.Commands
+{
+    public class ModifyUserCommand : IRequest<GetUserDto>
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
