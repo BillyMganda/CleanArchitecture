e8ac609c63a8b075c38a3da6ec9df60aece00b49
Billy <bimganda@gmail.com>
2023-03-30 13:04:59 -0400

    add command handler for modifying a user

---
+++ CleanArchitecture.Application/Handlers/CommandHandlers/ModifyUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchitecture.Application.Commands;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+
+namespace CleanArchitecture.Application.Handlers.CommandHandlers
+{
+    public class ModifyUserCommandHandler : IRequestHandler<ModifyUserCommand, User>
+    {
+        private readonly IUserRepository _userRepository;
+        public ModifyUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(ModifyUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Email = request.Email;
+
+            await _userRepository.UpdateAsync(user);
+
+            return user;
+        }
+    }
+}
