b098aa7cab276805af9235e6faa3e668279f48a5
Billy <bimganda@gmail.com>
2023-03-30 12:48:36 -0400

    add command to create user, should return GetUserDto object

---
+++ CleanArchitecture.Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.DTOs.Users;
+using MediatR;
+
+namespace CleanArchitecture.Application.Commands
+{
+    public class CreateUserCommand : IRequest<GetUserDto>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
