08d4083c93cf2e9b9fec8789cfe5580e67e8cf54
Billy <bimganda@gmail.com>
2023-03-30 12:44:08 -0400

    create user repository interface to perform CRUD operations

---
+++ CleanArchitecture.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.DTOs.Users;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<GetUserDto> GetByIdAsync(Guid id);
+        Task<GetUserDto> GetByEmailAsync(string email);
+        Task AddAsync(CreateUserDto user);
+        Task UpdateAsync(ModifyUserDto user);
+        Task DeleteAsync(Guid id);
+    }
+}
