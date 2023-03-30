54a27421c596bfd084bb11e276bb2c24a0c23617
Billy <bimganda@gmail.com>
2023-03-30 12:56:27 -0400

    add query to get a user by id

---
+++ CleanArchitecture.Application/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Queries
+{
+    internal class GetUserByIdQuery : IRequest<User>
+    {
+        public Guid Id { get; set; }
+    }
+}
