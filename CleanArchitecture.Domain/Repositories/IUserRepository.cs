afcb315c15920c2861d9532352c5f751fee9919b
Billy <bimganda@gmail.com>
2023-03-31 07:51:08 -0400

    implement IUserRepository in infra layer

--- CleanArchitecture.Domain/Repositories/IUserRepository.cs
+++ CleanArchitecture.Domain/Repositories/IUserRepository.cs
@@ -6,9 +6,9 @@ namespace CleanArchitecture.Domain.Repositories
     public interface IUserRepository
     {
         Task<User> GetByIdAsync(Guid id);
-        Task<User> GetByEmailAsync(string email);
-        Task AddAsync(User user);
-        Task UpdateAsync(User user);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> AddAsync(User user);
+        Task<User> UpdateAsync(User user);
         Task DeleteAsync(Guid id);
     }
 }
